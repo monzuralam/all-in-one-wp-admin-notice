5fe2ac182af8a1cf1c201660e3ffa75a65ee8671
monzuralam <monzur707@gmail.com>
2022-02-20 23:47:25 +0600

    initial setup

---
+++ all-in-one-wp-admin-notice.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Plugin Name:       All-in-One WP Admin Notice
+ * Plugin URI:        https://wordpress.org/plugins/all-in-one-wp-admin-notice
+ * Description:       All-in-One WP Admin Notice plugin will show custom notice in WordPress Dashboard.
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.0
+ * Author:            Monzur Alam
+ * Author URI:        https://profiles.wordpress.org/monzuralam
+ * Text Domain:       all-in-one-wp-admin-notice
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ */
+
+// If this file is called directly, abort.
+if (!defined('ABSPATH')) {
+    exit; // Exit if accessed directly.
+}
